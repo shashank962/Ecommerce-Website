9cee2b956ac329dac0d01b0786ae5e1684a202ae
shashank joshi <shashankjoshi1242000@gmail.com>
2022-01-13 17:42:07 +0530

    Hexaedge

--- HexaEdge/settings.py
+++ HexaEdge/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '$a^w$eh7g*&@dt%hfv7qu*a87u(b3$(5ng(f@bj@%9a*2_z+te'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [hexaedgenetproducts.herokuapp.com]
 
 
 # Application definition
